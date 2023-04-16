bee596f006d99044d6319e456e8319994a92c5fc
AndresGroselj <61983866+AndresGroselj@users.noreply.github.com>
2023-04-16 11:29:52 -0400

    feature WIP: download progress

--- Downloader.js
+++ Downloader.js
@@ -3,6 +3,7 @@ const drive = google.drive({ version: "v3", auth: "AIzaSyA13vMIhtNRKkPJnICBejqpo
 const path = require("path")
 const os = require("os");
 const fs = require('fs-extra');
+const Progress = require("./Progress")
 
 class Downloader {
   static async listDirectory(directoryId){
@@ -22,7 +23,7 @@ class Downloader {
       });
   }
 
-  static async downloadFile(fileId){
+  static async downloadFile(fileId, name){
       let fconf = {};
       fconf.fileId = fileId;
       fconf.alt = "media";
@@ -41,21 +42,46 @@ class Downloader {
           }
         );
       });
+
+      const p = path.join(os.tmpdir(), name);
+      console.log("saving!!! ######################")
+      console.log(p)
+      const dest = fs.createWriteStream(p);
+
+      let progress = 0
+      dest.on('data', (chunk) => {
+        progress += chunk.length /// fileSize
+        console.log('progress', progress)
+      })
+
+      stream.data.on('error', err => {
+        done(err);
+      }).on('end', ()=>{
+        
+      })
+      .pipe(dest);
   }
 
   static async saveFileStream(stream, name){
-    const p = path.join(os.tmpdir(), name);
-    console.log("saving!!! ######################")
-    console.log(p)
-    const dest = fs.createWriteStream(p);
-    stream.data.on('error', err => {
-      done(err);
-    }).on('end', ()=>{
-      
-    })
-    .pipe(dest);
+    
 
   }
+
+  static async getFileSize(fileId){
+    let fconf = {};
+    fconf.fileId = fileId;
+
+    return new Promise((resolve, reject) => 
+      drive.files.get(
+        fconf, 
+        function (err, metadata) {
+          if (err) return reject(err)
+          else resolve(metadata.size)
+        }
+      )
+    );
+  }
+
 }
 
 module.exports = Downloader;
